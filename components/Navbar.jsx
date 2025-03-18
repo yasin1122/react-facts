66b3b1ba27a03b5f32b030ad756d0f9a861f557b
Yasin Cakal <whoisyasin@gmail.com>
2025-03-17 21:36:20 -0700

    logo path

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,10 @@
+import logo from "../images/react-logo.png"
+
 export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="../images/react-logo.png" alt="React logo" />
+                <img src={logo} alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
