0fdffaf3c01c32b71caf46b609e727123a77e491
Yasin Cakal <whoisyasin@gmail.com>
2025-03-17 21:27:55 -0700

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="/images/react-logo.png" alt="React logo" />
+                <img src="images/react-logo.png" alt="React logo" />
                 <span>ReactFacts</span>
             </nav>
         </header>
